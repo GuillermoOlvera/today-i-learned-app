0027c718982d645d8f07c1873b6737e677ef400a
Guillermo <memolvpz@gmail.com>
2023-04-25 17:55:36 -0600

    created a loader component

--- src/TodayILearnedApp.jsx
+++ src/TodayILearnedApp.jsx
@@ -4,17 +4,23 @@ import { supabase } from "./data/superbase";
 import { CategoryFilter, FactList, NewFactForm, Header } from "./components";
 
 import "./style.css";
+import { Loading } from "./components/Loading";
 
 export const TodayILearned = () => {
 
   const [showForm, setShowForm] = useState(false);
   const [facts, setFacts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const getFacts = async () => {
+      setIsLoading(true);
       let { data: facts, error } = await supabase
-        .from('facts').select('*')
-        setFacts(facts);
+        .from('facts').select('*').order('votesInteresting', { ascending: false }).limit(1000)
+
+        if(!error) setFacts(facts);
+        else alert('There was a problem getting data');
+        setIsLoading(false);
     }
     getFacts();
   },[]);
@@ -25,8 +31,11 @@ export const TodayILearned = () => {
       {/* use state variable */}
       { showForm && <NewFactForm setFacts={ setFacts } setShowForm={ setShowForm }/> }
       <main className="main">
-        <CategoryFilter />
-        <FactList facts={ facts }/>
+      <CategoryFilter />
+        {
+          isLoading ? <Loading /> : <FactList facts={ facts }/>
+        }
+        
       </main>
     </>
   );
