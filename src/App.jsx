a379fd771d20909bb664628b7eace50bfe32031c
Guillermo <memolvpz@gmail.com>
2023-04-24 23:03:29 -0600

    rendering the list of facts

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,30 @@
-import { CategoryFilter } from './components/CategoryFilter';
-import { FactList } from './components/FactList';
-import { NewFactForm } from './components/NewFactForm';
-import './style.css';
+import { CategoryFilter } from "./components/CategoryFilter";
+import { FactList } from "./components/FactList";
+import { NewFactForm } from "./components/NewFactForm";
+import "./style.css";
 
 export const App = () => {
-
   return (
     <>
-     <header className="header">
-            <div className="logo">
-                <img 
-                    src="logo.png"
-                    alt="Today I Learned Logo" 
-                    height="68" 
-                    width="68"
-                />
-                <h1>Today I Learned!</h1>
-            </div>
-            
-            <button className="btn btn-large btn-open">Shared a fact</button>
-        </header>
+      <header className="header">
+        <div className="logo">
+          <img
+            src="logo.png"
+            alt="Today I Learned Logo"
+            height="68"
+            width="68"
+          />
+          <h1>Today I Learned!</h1>
+        </div>
 
-        <NewFactForm />
-        <main className='main'>
-          <CategoryFilter />
-          <FactList />
-        </main>
+        <button className="btn btn-large btn-open">Shared a fact</button>
+      </header>
+
+      <NewFactForm />
+      <main className="main">
+        <CategoryFilter />
+        <FactList />
+      </main>
     </>
-  )
-}
+  );
+};
--- src/components/FactList.jsx
+++ src/components/FactList.jsx
@@ -1,5 +1,38 @@
+import { CATEGORIES, initialFacts } from "../data";
+
 export const FactList = () => {
   return (
-    <div>FactList</div>
-  )
-}
+    <section>
+      <ul className="facts-list">
+        {
+          initialFacts.map((fact) => (
+            <li className="fact" key={ fact.id }>
+              <p>
+                { fact.text }
+                <a
+                  className="source"
+                  href={ fact.source }
+                  target="_blank"
+                >
+                  (Source)
+                </a>
+              </p>
+              <span 
+                className="tag" 
+                style={{ 
+                  backgroundColor: CATEGORIES.find((cat) => cat.name === fact.category).color
+                }}
+              >
+                { fact.category }
+              </span>
+              <div className="vote-buttons">
+                <button>👍 { fact.votesInteresting }</button>
+                <button>🤯 { fact.votesMindblowing }</button>
+                <button>⛔️ { fact.votesFalse }</button>
+              </div>
+            </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
