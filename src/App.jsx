5d04e6965997b12202c560817f46113e4e080410
Guillermo <memolvpz@gmail.com>
2023-04-24 22:29:03 -0600

    React library added

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './style.css';
+
+export const App = () => {
+
+  return (
+    <>
+     <header className="header">
+            <div className="logo">
+                <img 
+                    src="logo.png"
+                    alt="Today I Learned Logo" 
+                    height="68" 
+                    width="68"
+                />
+                <h1>Today I Learned!</h1>
+            </div>
+            
+            <button className="btn btn-large btn-open">Shared a fact</button>
+        </header>
+    </>
+  )
+}
